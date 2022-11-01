bbccf35285544669e6fbab928d364c8007bdb94c
Danara Tagayeva <danara.tagayeva@gmail.com>
2022-10-31 20:51:37 -0400

    footer component done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem 0;
+  color: gray;
+`;
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Container>Copyright © 2022 Dana Tagayeva. All rights reserved.</Container>
+  );
 };
 
 export default Footer;
